f270eca9273ec721c4d3c097008263506e00816e
kuriyantoadi <melatikuning212@gmail.com>
2021-05-21 14:42:16 +0700

    update 21 mei 2021, update 6

---
+++ dashboard1.php
@@ -0,0 +1,22 @@
+<?php include('header.php') ?>
+		<!-- END LEFT SIDEBAR -->
+		<!-- MAIN -->
+		<div class="main">
+			<!-- MAIN CONTENT -->
+			<div class="main-content">
+				<div class="container-fluid">
+					<!-- <h3 class="page-title">Halaman Dashboard</h3> -->
+					<div class="panel panel-headline">
+						<div class="panel-body">
+              <center> 
+
+							<h1>Selamat Datang di Sistem Informasi Sekolah</h1>
+							<h4>Anda Berhasil Login Sebagai Siswa</h4>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- END MAIN CONTENT -->
+		</div>
+		<!-- END MAIN -->
+<?php include('footer.php') ?>
